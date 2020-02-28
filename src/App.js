02bff6dcf4c9ca88532a3d772a9aea3512b0f565
sagarsalyan <sagarslyn@gmail.com>
2020-02-28 11:51:43 +0530

    FirstCommit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Create from './components/Create'
+import Edit from './components/Edit'
+import Index from './components/Index'
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand">React CRUD Example</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/index'} className="nav-link">Index</Link>
+                </li>
+              </ul>
+            </div>
+          </nav> <br/>
+          <h2>Welcome to React CRUD Tutorial</h2> <br/>
+          <Switch>
+              <Route exact path='/create' component={ Create } />
+              <Route path='/edit/:id' component={ Edit } />
+              <Route path='/index' component={ Index } />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
